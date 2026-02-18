54271bf3cd7d984d6fdf668a6d15aa68d77ecfd3
ali-zamani <azamani081@gmail.com>
2026-02-18 14:44:39 +0330

    update user schema

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -1,29 +1,41 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, 'نام کاربری اجباری است'],
-        unique: true,
-        trim: true,
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'نام  اجباری است'],
+            unique: true,
+            trim: true,
+        },
+        username: {
+            type: String,
+            required: [true, 'نام کاربری اجباری است'],
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+        role: {
+            type: String,
+            enum: ['user', 'admin'],
+            default: 'user',
+            message: '{VALUE} is not supported',
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        lowercase: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-    },
-    role: {
-        type: String,
-        enum: ['user', 'admin'],
-        default: 'user',
-        message: '{VALUE} is not supported',
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
 const User = mongoose.model('User', userSchema);
 
