30e2329df2222d2fa54c25845ad9af21dddc0b36
ali-zamani <azamani081@gmail.com>
2026-02-18 17:09:22 +0330

    fix this bug

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,8 +1,15 @@
 const userService = require('../service/userService');
+const autoBind = require('auto-bind');
 
 class userController {
+    constructor() {
+        autoBind(this);
+    }
     createUser(req, res) {
         userService.createNewUser(req, res);
+    }
+    findUserByUserName(req, res) {
+        userService.findUserByUsername(req, res);
     }
 }
 module.exports = new userController();
