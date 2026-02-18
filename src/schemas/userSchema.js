337773954ad045d94aa84f38d7f5eb13efd35d31
ali-zamani <azamani081@gmail.com>
2026-02-18 15:54:57 +0330

    Password Saved as Hashed with bcrypt

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
     {
@@ -33,9 +34,15 @@ const userSchema = new mongoose.Schema(
         },
     },
     {
+        // when add this field by default create at is now!
         timestamps: true,
     }
 );
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 
