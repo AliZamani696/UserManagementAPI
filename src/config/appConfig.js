a46d88079378f60618da953304039ebcab20d753
ali-zamani <azamani081@gmail.com>
2026-02-18 14:54:47 +0330

    create config for load .env file & app database

---
+++ src/config/appConfig.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+class AppConfig {
+    constructor() {
+        this.port = process.env.SERVER_PORT || 9090;
+        const user = process.env.DB_USER_NAME || 'root';
+        const pass = process.env.DB_PASSWORD || '123';
+        const uri = process.env.DB_URI
+            ? process.env.DB_URI.replace('mongodb://', '')
+            : '172.20.6.7:27017/';
+        const dbName = process.env.DB_NAME || 'userManageApi';
+        this.dbUri = `mongodb://${user}:${pass}@${uri}${dbName}?authSource=admin`;
+    }
+}
+
+module.exports = new AppConfig();
