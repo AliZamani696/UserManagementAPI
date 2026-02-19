b5425f9e76f52d955df6621c2dfbba805a97e189
AliZamani696 <azamani081@gmail.com>
2026-02-19 20:47:26 +0330

    add authMiddleware for refresh token route

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const authController = require('./../controller/authCotroller');
 const authValidator = require('./../validators/authValidator');
+const authMiddleware = require('./../middleware/authMiddleware');
 
+router.post('/refresh', authMiddleware(), authController.refreshToken);
 router.post('/login', authValidator.loginValidation(), authController.login);
 router.post(
     '/register',
     authValidator.registerValidation(),
     authController.register
 );
+router.post('/logout', authController.logout);
 
 module.exports = router;
