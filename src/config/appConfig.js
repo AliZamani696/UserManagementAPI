ff42e2ccd0a87f0cb30ef404ef11fb5ff8970dfc
Ali Zamani <166255379+AliZamani696@users.noreply.github.com>
2026-02-21 08:49:30 +0330

    fix Update appConfig.js

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -7,7 +7,7 @@ class AppConfig {
     // const pass = process.env.DB_PASSWORD || '123';
     // const uri = process.env.DB_URI
     //     ? process.env.DB_URI.replace('mongodb://', '')
-    //     : '172.20.6.7:27017/';
+    //     : '';
     // const dbName = process.env.DB_NAME || 'userManageApi';
     // this.dbUri = `mongodb://${user}:${pass}@${uri}${dbName}?authSource=admin`;
     this.dbUri = process.env.DB_URI || 'mongodb://localhost:27017/userManageApi';
